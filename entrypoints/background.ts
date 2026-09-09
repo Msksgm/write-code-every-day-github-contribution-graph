bf9478120108bc540ade2bd96d1e42f71b270b0c
Msksgm <msksgm3124@gmail.com>
2026-09-10 07:54:58 +0900

    feat: GitHub APIからリポジトリ情報を取得

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,22 @@
 export default defineBackground(() => {
   console.log('Hello background!', { id: browser.runtime.id });
+  fetchRepository().catch((error) => {
+    console.error('リポジトリの取得エラー:', error);
+  })
 });
+
+const fetchRepository = async (): Promise<void> => {
+  const response = await fetch('https://api.github.com/repos/Msksgm/write-code-every-day-github-contribution-graph');
+
+  if (!response.ok) {
+    throw new Error(`取得に失敗しました : HTTP ${response.status}`);
+  }
+
+  const repository = await response.json();
+
+  console.log({
+    full_name: repository.full_name,
+    private: repository.private,
+    default_branch: repository.default_branch,
+  });
+};
--- wxt.config.ts
+++ wxt.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  manifest: {
+    host_permissions: [
+      'https://api.github.com/*'
+    ]
+  },
   modules: ['@wxt-dev/module-react'],
 });
