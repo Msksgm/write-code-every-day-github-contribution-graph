5bd02fe0bd4d9d433074916532eb1a9985fdd308
Msksgm <msksgm3124@gmail.com>
2026-09-10 07:21:31 +0900

    refactor: id で write-code-every-day が重複しないように変更

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,7 +1,12 @@
 export default defineContentScript({
   matches: ['https://github.com/Msksgm'],
   main() {
+    const id = document.getElementById('write-code-every-day-root')
+    if (id !== null) {
+      return
+    }
     const element = document.createElement('div');
+    element.id = 'write-code-every-day-root';
     element.textContent = 'Write Code Every Day';
     document.body.prepend(element);
   },
